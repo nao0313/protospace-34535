79993235c5ee516b262ccf093d9f618aa82516b3
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-18 18:14:42 +0900

    画像から詳細ページへ遷移の修正・トップページへリダイレクトの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -51,7 +51,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless @prototype_id
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
